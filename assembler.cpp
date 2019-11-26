47740053427912ef2c5fcd1b70abb174f0de8b50
Your NameLukasz <you@example.comlnozewski@dundee.ac.uk>
2019-11-26 13:35:37 +0000

    2.

--- assembler.cpp
+++ assembler.cpp
@@ -174,9 +174,140 @@ void Assembler::importfile(string selection){
 					cout << " Converting line: " << readLine << endl;
 					interpretateLine(readLine);
 				}
+				else if(selection == "second"){
+					string label;
+					for(int j = 11; j < 16; j++){
+						if(readLine.at(j) == 32 || readLine.at(j) == 58){
+							j = 15;
+						}
+						else{
+							label = label + readLine.at(j);
+						}
+					}
+				}
+				for(int h = 0; h < Symbols.size(); h++){
+					if(Symnols.at(h).label == label){
+						string binaryLineno = convertToBinary(Symbols.at(h).lineNumber);
+
+						for(int k = 0; k < 5; k++){
+							tempStore[k+5][linesLoaded] = binaryLineno.at(k);
+						}
+						h = Symbols.size();
+					}
+				}
 			}
-
-
 		}
+	}
+}
+
+string Assembler::convertOpcode (string code){
+	string convertCode="Null";
+
+	/*
+	- S = contents of that store location
+	- CI = contents of control instruction
+	- A = contents of accumulator
+	*/
+	if (code == "JMP") // CI = S
+	{
+		convertCode="000";
+	}
+
+	else if (code == "JRP") // CI = CI + S ()
+	{
+		convertCode="100";
+	}
+
+	else if (code == "LDN") // A = -S
+	{
+		convertCode="010";
+	}
+
+	else if (code == "STO") // S = A
+	{
+		convertCode="110";
+	}
+
+	else if (code == "SUB") // A = A - S
+	{
+		convertCode="001";
+	}
+
+	else if (code == "NUM") // Num
+	{
+		convertCode="101";
+	}
+
+	else if (code == "CMP") // if A<O then CI = CI + 1
+	{
+		convertCode="011";
+	}
+
+	else if (code == "STP") //to stop
+	{
+		convertCode="111";
+	}
+
+	return covertCode;
+}
+
+section	.text
+   global _start     ;must be declared for linker (ld)
+
+_start:	            ;tells linker entry point
+   mov	edx,len     ;message length
+   mov	ecx,msg     ;message to write
+   mov	ebx,1       ;file descriptor (stdout)
+   mov	eax,4       ;system call number (sys_write)
+   int	0x80        ;call kernel
+
+   mov	eax,1       ;system call number (sys_exit)
+   int	0x80        ;call kernel
+
+section	.data
+msg db 'Hello, world!', 0xa  ;string to be printed
+len equ $ - msg     ;length of the string
+
+
+void Assembler::insertInstruction(string label)
+{
+	// Inserting Start
+	if(label=="START" && currentLineNumber==0)
+	{
+		string line0 =  "0000:00000000000000000000000000000000";
+		for(int i =0; i < 37; i++)
+		{
+					tempStore[i][currentLineNumber]=line0.at(i);
+		}
+
+		currentLineNumber++;
+	}
+
+	if(label=="STOP")
+	{
+		string lineNo;
+		stringstream out;
+		out << currentLineNumber;
+		lineNo = out.str();
+
+		if(currentLineNumber<10)
+		{
+			string linex =  "000" + lineNo + ":00000000000001110000000000000000";
+			for(int i =0; i < 37; i++)
+			{
+				tempStore[i][currentLineNumber]=linex.at(i);
+			}
+		}
+		else
+		{
+			string linex =  "00" + lineNo + ":00000000000001110000000000000000";
+			for(int i =0; i < 37; i++)
+			{
+				tempStore[i][currentLineNumber]=linex.at(i);
+			}
+		}
+	}
+	if(displayProgressBar==true){
+		displayProgress();
 	}
 }
